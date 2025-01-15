0dfcef51990b90976e6b632cdf8e1e189c03823d
Victor de Souza Garcia <victor.dsgw@gmail.com>
2025-01-15 20:51:35 -0300

    Mobile Menu

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -2,16 +2,21 @@ import Navbar from "@/components/navgation/Navbar";
 import { Button } from "@/components/ui/button";
 import { useScrollSpy } from "@/components/navgation/ScrollSpy";
 import Link from "next/link";
+import MobileMenu from "@/components/navgation/MobileMenu";
+import { useState } from "react";
+import Hamburger from "hamburger-react";
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     const sectionIds = ["contact"];
     const activeSection = useScrollSpy(sectionIds);
 
     return (
-        <header className="fixed top-0 left-0 w-screen md:h-[var(--navbar-height)] flex flex-row justify-items-center bg-slate-900 pl-1 pr-3 py-3 xl:py10 2xl:py-16">
-            <div className="container mx-auto flex justify-around lg:justify-between">
+        <header className="fixed top-0 left-0 w-screen md:h-[var(--navbar-height)] flex flex-row bg-slate-900 pl-1 pr-3 py-3 xl:py10 2xl:py-16">
+            <div className="container mx-auto flex justify-between lg:justify-between">
                 <Link href="/" className="flex items-center">
-                    <h1 className="text-base md:text-2xl lg:text-3xl 2xl:text-4xl font-semibold xl:font-bold 2xl:font-extrabold">Victor
+                    <h1 className="text-2xl md:text-3xl lg:text-4xl 2xl:text-5xl font-semibold xl:font-bold 2xl:font-extrabold">Victor
                         <span className="text-purple-500"> DSG</span>
                     </h1>
                 </Link>
@@ -31,7 +36,10 @@ export default function Header() {
                     ))}
                 </div>
 
-                <div className="lg:hidden flex items-center">moba</div>
+                <div className="lg:hidden flex items-center">
+                    <Hamburger toggled={menuOpen} toggle={setMenuOpen} />
+                    <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+                </div>
             </div>
         </header>
     );
