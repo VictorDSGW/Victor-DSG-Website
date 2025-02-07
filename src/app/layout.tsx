2b611d7769994d427e178bb7491ce8bcf448206a
Victor de Souza Garcia <victor.dsgw@gmail.com>
2025-02-07 10:54:53 -0300

    Contact Section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from "next";
-import { Jura } from 'next/font/google'
+import { Courier_Prime, Jura } from 'next/font/google'
 import "@/app/globals.css";
 
 const jura = Jura({
   subsets: ['latin'],
   weight: ["300", "400", "500", "600", "700"],
   variable: '--font-jura'
+})
+
+const typeWriter = Courier_Prime({
+  subsets: ['latin'],
+  weight: ["400", "700"],
+  variable: '--font-typeWriter'
 })
 
 
@@ -36,7 +42,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-br" className={jura.className}>
+    <html lang="pt-br" className={jura.className || typeWriter.className}>
       <body>
         {children}
       </body>
